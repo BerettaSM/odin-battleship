e8e68e6db5785bf61cc71664a72ae642b76347bd
Ramon Saviato <ramonsaviato@hotmail.com>
2024-08-01 23:30:48 -0300

    fix: adjust webpack pattern match to not include test files

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /(?<!\.spec)\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
